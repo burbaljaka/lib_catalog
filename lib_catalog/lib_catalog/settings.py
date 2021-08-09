4c3422960189594eb3212f8e5392ef2292aa9a91
Timur Kapitonov <timur.kapitonov@advinow.com>
2021-08-09 15:51:12 +0300

    rolled to postgres

--- lib_catalog/lib_catalog/settings.py
+++ lib_catalog/lib_catalog/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'lib_catalog.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "django",
+        "USER": "django",
+        "PASSWORD": "django",
+        "HOST": "database",
+        "PORT": "5432",
+        "CONN_MAX_AGE": None
+    },
 }
 
 
